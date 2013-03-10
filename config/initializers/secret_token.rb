bde483cc26998aa5e11858c6ac1a4fcc532293cb
cameel <cameel2@gmail.com>
2013-03-10 01:58:31 +0100

    Aplikacja wygenerowana automatycznie przez rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Photowall::Application.config.secret_token = '3bce92446f79c65eb6c3bb58f3de9e41a29e2b08ebb3dfa49a027ca6356af562fa1730cef5ce688ff03bf038076e57f2a485f227820a546677c16a78ff347eec'
